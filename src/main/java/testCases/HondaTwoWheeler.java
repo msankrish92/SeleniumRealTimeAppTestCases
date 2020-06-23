b26b2372a40c40dc25573e9333c4142b982cfb90
msankrish92 <33427950+msankrish92@users.noreply.github.com>
2020-06-23 14:42:27 +0530

    comparing the Map

--- src/main/java/testCases/HondaTwoWheeler.java
+++ src/main/java/testCases/HondaTwoWheeler.java
@@ -40,19 +40,37 @@ public class HondaTwoWheeler {
 		builder.moveToElement(driver.findElementByLinkText("ENGINE")).perform();
 
 //			4) Put all the details as key and value into Map
-		List<WebElement> engineSpecList = driver.findElementsByXPath("(//ul[@class='tab_content'])[2]/li/span");
+		List<WebElement> dioEngineSpecList = driver.findElementsByXPath("(//ul[@class='tab_content'])[2]/li/span");
 
-		System.out.println(engineSpecList.get(0).getText());
-		Map<String,String> engineKeyValue = new LinkedHashMap<String, String>();
-		
-		for (int i = 0; i < engineSpecList.size()-2; i=i+2) {
-			engineKeyValue.put(engineSpecList.get(i+1).getText(), engineSpecList.get(i+2).getText());
+		System.out.println(dioEngineSpecList.get(0).getText());
+		Map<String, String> dioEngineKeyValue = new LinkedHashMap<String, String>();
+
+		for (int i = 0; i < dioEngineSpecList.size() - 2; i = i + 2) {
+			dioEngineKeyValue.put(dioEngineSpecList.get(i + 1).getText(), dioEngineSpecList.get(i + 2).getText());
 		}
-		System.out.println(engineKeyValue);
-		
+		System.out.println(dioEngineKeyValue);
+
 //			5) Go to Scooters and click Activa 125
+		driver.findElementByLinkText("Scooter").click();
+		driver.findElementByXPath("(//div[@id='scooter']//div[@class='item'])[3]").click();
+		driver.findElementByLinkText("Specifications").click();
+//		Actions builder = new Actions(driver);
+		Thread.sleep(3000);
+		builder.moveToElement(driver.findElementByLinkText("ENGINE")).perform();
+
 //			6) Put All its Engine Specification into another Map same as like dio
+		List<WebElement> activaEngineSpecList = driver.findElementsByXPath("(//ul[@class='tab_content'])[2]/li/span");
+		Map<String, String> activaEngineKeyValue = new LinkedHashMap<String, String>();
+
+		for (int i = 0; i < activaEngineSpecList.size() - 2; i = i + 2) {
+			activaEngineKeyValue.put(activaEngineSpecList.get(i + 1).getText(), activaEngineSpecList.get(i + 2).getText());
+		}
+		System.out.println(activaEngineKeyValue);
+
 //			7) Compare Dio and Activa Maps and print the different values of the samekeys.
+		
+		
+		
 //			9) Click FAQ from Menu and Click dio under Browse By Product
 //			10) Click  Vehicle Price and Select scooter, Dio BS-VI from the dropdown and click submit
 //			11) click the price link,  Go to the new Window and select the state, city
